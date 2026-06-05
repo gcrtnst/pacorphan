46604b7cb7906a371a5cc49b4045eab2d0af9f4e
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-06-06 00:48:50 +0900

    Add handle validation to newDB

--- internal/alpm/db.go
+++ internal/alpm/db.go
@@ -3,6 +3,7 @@ package alpm
 // #cgo pkg-config: libalpm
 // #include <alpm.h>
 import "C"
+import "runtime"
 
 type DB struct {
 	h *Handle
@@ -10,6 +11,13 @@ type DB struct {
 }
 
 func newDB(h *Handle, c_db *C.alpm_db_t) *DB {
+	c_handle := C.alpm_db_get_handle(c_db)
+	runtime.KeepAlive(h)
+
+	if c_handle != h.c {
+		panic("alpm: database handle mismatch")
+	}
+
 	return &DB{h: h, c: c_db}
 }
 
