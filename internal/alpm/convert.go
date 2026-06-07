795b87550b777e52f52a4a7a7749e938a50b96b0
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-06-08 02:39:33 +0900

    Fix overflow check conditions in conversion functions

--- internal/alpm/convert.go
+++ internal/alpm/convert.go
@@ -12,14 +12,14 @@ func c2goInt(x C.int) int {
 	if C.INT_MIN <= math.MinInt {
 		gmin := math.MinInt
 		cmin := C.int(gmin)
-		if x <= cmin {
+		if x < cmin {
 			panic(fmt.Sprintf("C.int value %d overflows int", x))
 		}
 	}
 	if C.INT_MAX >= math.MaxInt {
 		gmax := math.MaxInt
 		cmax := C.int(gmax)
-		if x >= cmax {
+		if x > cmax {
 			panic(fmt.Sprintf("C.int value %d overflows int", x))
 		}
 	}
@@ -30,7 +30,7 @@ func c2goSize(x C.size_t) int {
 	if C.SIZE_MAX >= math.MaxInt {
 		gmax := math.MaxInt
 		cmax := C.size_t(gmax)
-		if x >= cmax {
+		if x > cmax {
 			panic(fmt.Sprintf("C.size_t value %d overflows int", x))
 		}
 	}
