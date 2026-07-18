d144d3a81a49954ce80912ec6ba7e03bbd71a3ab
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-19 02:02:52 +0900

    Add unit tests for zero-value Handle methods

--- internal/alpm/handle_test.go
+++ internal/alpm/handle_test.go
@@ -11,8 +11,27 @@ func TestNilHandleClose(t *testing.T) {
 	}
 }
 
+func TestZeroHandleClose(t *testing.T) {
+	h := &Handle{}
+
+	err := h.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestNilHandleRoot(t *testing.T) {
 	h := (*Handle)(nil)
+
+	got := h.Root()
+	const want = ""
+	if got != want {
+		t.Errorf("h.Root() = %q, want %q", got, want)
+	}
+}
+
+func TestZeroHandleRoot(t *testing.T) {
+	h := &Handle{}
 
 	got := h.Root()
 	const want = ""
@@ -31,8 +50,28 @@ func TestNilHandleDBPath(t *testing.T) {
 	}
 }
 
+func TestZeroHandleDBPath(t *testing.T) {
+	h := &Handle{}
+
+	got := h.DBPath()
+	const want = ""
+	if got != want {
+		t.Errorf("h.DBPath() = %q, want %q", got, want)
+	}
+}
+
 func TestNilHandleLocalDB(t *testing.T) {
 	h := (*Handle)(nil)
+
+	got := h.LocalDB()
+	want := (*DB)(nil)
+	if got != want {
+		t.Errorf("h.LocalDB() = %#v, want %#v", got, want)
+	}
+}
+
+func TestZeroHandleLocalDB(t *testing.T) {
+	h := &Handle{}
 
 	got := h.LocalDB()
 	want := (*DB)(nil)
