c466cea7d7a62631e37367d67d2d8a0cf2f01221
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-18 23:47:21 +0900

    Add test helper functions

---
+++ internal/alpmtest/helper.go
@@ -0,0 +1,24 @@
+package main
+
+func HelpEnv(t *T) *Env {
+	env, err := NewEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		err := env.Dispose()
+		if err != nil {
+			t.Log(err)
+		}
+	})
+
+	return env
+}
+
+func HelpMakeAndInstall(t *T, env *Env, src *PkgBuild, explicit bool) {
+	err := env.MakeAndInstall(src, explicit)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
