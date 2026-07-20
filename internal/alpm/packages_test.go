858e4ee9878a93adb5c63f17ff725f531f209293
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-21 00:34:49 +0900

    Add tests for nil and zero Pkg values

--- internal/alpm/packages_test.go
+++ internal/alpm/packages_test.go
@@ -2,6 +2,106 @@ package alpm
 
 import "testing"
 
+func TestNilPkgName(t *testing.T) {
+	pkg := (*Pkg)(nil)
+
+	got := pkg.Name()
+	const want = ""
+	if got != want {
+		t.Errorf("pkg.Name() = %q, want %q", got, want)
+	}
+}
+
+func TestZeroPkgName(t *testing.T) {
+	pkg := &Pkg{}
+
+	got := pkg.Name()
+	const want = ""
+	if got != want {
+		t.Errorf("pkg.Name() = %q, want %q", got, want)
+	}
+}
+
+func TestNilPkgVersion(t *testing.T) {
+	pkg := (*Pkg)(nil)
+
+	got := pkg.Version()
+	const want = ""
+	if got != want {
+		t.Errorf("pkg.Version() = %q, want %q", got, want)
+	}
+}
+
+func TestZeroPkgVersion(t *testing.T) {
+	pkg := &Pkg{}
+
+	got := pkg.Version()
+	const want = ""
+	if got != want {
+		t.Errorf("pkg.Version() = %q, want %q", got, want)
+	}
+}
+
+func TestNilPkgDepends(t *testing.T) {
+	pkg := (*Pkg)(nil)
+
+	got := pkg.Depends()
+	want := (*List[*Depend])(nil)
+	if got != want {
+		t.Errorf("pkg.Depends() = %#v, want %#v", got, want)
+	}
+}
+
+func TestZeroPkgDepends(t *testing.T) {
+	pkg := &Pkg{}
+
+	got := pkg.Depends()
+	want := (*List[*Depend])(nil)
+	if got != want {
+		t.Errorf("pkg.Depends() = %#v, want %#v", got, want)
+	}
+}
+
+func TestNilPkgOptDepends(t *testing.T) {
+	pkg := (*Pkg)(nil)
+
+	got := pkg.OptDepends()
+	want := (*List[*Depend])(nil)
+	if got != want {
+		t.Errorf("pkg.OptDepends() = %#v, want %#v", got, want)
+	}
+}
+
+func TestZeroPkgOptDepends(t *testing.T) {
+	pkg := &Pkg{}
+
+	got := pkg.OptDepends()
+	want := (*List[*Depend])(nil)
+	if got != want {
+		t.Errorf("pkg.OptDepends() = %#v, want %#v", got, want)
+	}
+}
+
+func TestNilPkgReason(t *testing.T) {
+	pkg := (*Pkg)(nil)
+
+	got := pkg.Reason()
+	const want = PkgReasonUnknown
+	if got != want {
+		t.Errorf("pkg.Reason() = %d, want %d", got, want)
+	}
+}
+
+func TestZeroPkgReason(t *testing.T) {
+	pkg := &Pkg{}
+
+	got := pkg.Reason()
+	const want = PkgReasonUnknown
+	if got != want {
+		t.Errorf("pkg.Reason() = %d, want %d", got, want)
+	}
+}
+
 func TestCompareVersion(t *testing.T) {
 	tt := []struct {
 		in1  string
