1226ede461f092b10144a5d0e2554edf180ee7f0
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-23 22:46:14 +0900

    Add TestDepend

---
+++ internal/alpmtest/depends.go
@@ -0,0 +1,60 @@
+package main
+
+import "github.com/gcrtnst/pacorphan/internal/alpm"
+
+func init() { Register("TestDepend", TestDepend) }
+func TestDepend(t *T) {
+	env := HelpEnv(t)
+
+	srcA := NewPkgBuild("a", "0.0.1")
+	HelpMakeAndInstall(t, env, srcA, false)
+
+	srcB := NewPkgBuild("b", "0.0.2")
+	srcB.Depends = append(srcB.Depends, "a=0.0.1")
+	HelpMakeAndInstall(t, env, srcB, true)
+
+	h, errHandle := alpm.NewHandle(env.Root, env.DBPath)
+	if errHandle != nil {
+		t.Fatal(errHandle)
+	}
+	defer func() {
+		err := h.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	db := h.LocalDB()
+	pkgs, errPkgs := db.PkgCache()
+	if errPkgs != nil {
+		t.Fatal(errPkgs)
+	}
+
+	pkgB := (*alpm.Pkg)(nil)
+	for pkg := range pkgs.All() {
+		if pkg.Name() == "b" {
+			pkgB = pkg
+			break
+		}
+	}
+	if pkgB == nil {
+		t.Fatal("package b not found")
+	}
+
+	dep := pkgB.Depends().Front().Data()
+
+	depStr := dep.String()
+	if depStr != srcB.Depends[0] {
+		t.Errorf("dep.String() = %q, want %q", depStr, srcB.Depends[0])
+	}
+
+	errClose := h.Close()
+	if errClose != nil {
+		t.Fatal(errClose)
+	}
+
+	depStrClosed := dep.String()
+	if depStrClosed != "" {
+		t.Errorf(`after close: dep.String() = %q, want ""`, depStrClosed)
+	}
+}
