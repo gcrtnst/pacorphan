811a0eadfab80392aa08afad7a32bf08a2ff2810
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-25 00:56:40 +0900

    Suppress makepkg and pacman stderr on success in tests

--- internal/alpmtest/helper.go
+++ internal/alpmtest/helper.go
@@ -1,5 +1,7 @@
 package main
 
+import "bytes"
+
 func HelpEnv(t *T) *Env {
 	env, err := NewEnv()
 	if err != nil {
@@ -18,8 +20,24 @@ func HelpEnv(t *T) *Env {
 }
 
 func HelpMakeAndInstall(t *T, env *Env, src *PkgBuild, explicit bool) {
+	we1 := env.Stdout
+	we2 := env.Stderr
+	wm1 := env.MakePkg.Stdout
+	wm2 := env.MakePkg.Stderr
+	defer func() {
+		env.Stdout = we1
+		env.Stderr = we2
+		env.MakePkg.Stdout = wm1
+		env.MakePkg.Stderr = wm2
+	}()
+
+	buf := new(bytes.Buffer)
+	env.SetStdout(nil)
+	env.SetStderr(buf)
+
 	err := env.MakeAndInstall(src, explicit)
 	if err != nil {
+		t.Output().Write(buf.Bytes())
 		t.Fatal(err)
 	}
 }
