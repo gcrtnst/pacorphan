b834d05022fa4de007d18c9ef619f2f4698e469d
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-25 15:51:09 +0900

    Add TestErrorALPMInit

--- internal/pacorphantest/test.go
+++ internal/pacorphantest/test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"errors"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -66,6 +67,47 @@ func TestErrorFlagParse(t *testcmd.T) {
 	}
 
 	stderrRe := regexp.MustCompile(`^error: .+\n$`)
+	if !stderrRe.Match(stderr.Bytes()) {
+		t.Errorf("exec [%s]: stderr.Bytes() = %q, want regexp %q", strings.Join(cmd.Args, " "), stderr.Bytes(), stderrRe.String())
+	}
+}
+
+func init() { testMain.Register("TestErrorALPMInit", TestErrorALPMInit) }
+func TestErrorALPMInit(t *testcmd.T) {
+	env := testcmd.HelpEnv(t)
+
+	errRemove := os.RemoveAll(env.DBPath)
+	if errRemove != nil {
+		t.Fatal(errRemove)
+	}
+
+	cmd := &exec.Cmd{
+		Path: pacorphan,
+		Args: []string{"pacorphan", "--sysroot", env.Root},
+	}
+
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	err := cmd.Run()
+	const wantCode = 1
+	if e, ok := errors.AsType[*exec.ExitError](err); ok {
+		if e.ExitCode() != wantCode {
+			t.Errorf("exec [%s]: exit code = %d, want %d", strings.Join(cmd.Args, " "), e.ExitCode(), wantCode)
+		}
+	} else if err != nil {
+		t.Error(testcmd.WrapExitError(cmd, err))
+	} else {
+		t.Errorf("exec [%s]: exit code = 0, want %d", strings.Join(cmd.Args, " "), wantCode)
+	}
+
+	if stdout.Len() != 0 {
+		t.Errorf("exec [%s]: stdout.Len() = %d, want 0", strings.Join(cmd.Args, " "), stdout.Len())
+	}
+
+	stderrRe := regexp.MustCompile(`^error: failed to initialize alpm library: .+\n$`)
 	if !stderrRe.Match(stderr.Bytes()) {
 		t.Errorf("exec [%s]: stderr.Bytes() = %q, want regexp %q", strings.Join(cmd.Args, " "), stderr.Bytes(), stderrRe.String())
 	}
