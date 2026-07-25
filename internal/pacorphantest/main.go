4afdef02f90877d2d6ba8d1bd764ddb1a473ec7d
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-25 12:39:54 +0900

    Add internal/pacorphantest binary for testing

---
+++ internal/pacorphantest/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/gcrtnst/pacorphan/internal/testcmd"
+	"github.com/spf13/pflag"
+)
+
+var pacorphan = ""
+var testMain = testcmd.NewTestMain()
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	var cmd string
+	fs := pflag.NewFlagSet("pacorphantest", pflag.ContinueOnError)
+	fs.StringVarP(&cmd, "cmd", "c", "", "path to pacorphan binary")
+
+	errParse := fs.Parse(os.Args[1:])
+	if errParse != nil {
+		if errors.Is(errParse, pflag.ErrHelp) {
+			return 0
+		}
+		fmt.Fprintf(os.Stderr, "error: %s\n", errParse)
+		return 2
+	}
+
+	if cmd == "" {
+		var err error
+		cmd, err = exec.LookPath("pacorphan")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			return 1
+		}
+	}
+	pacorphan = cmd
+
+	return testMain.Run()
+}
