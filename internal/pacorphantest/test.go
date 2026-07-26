b80c698ecb16f4307bbd3ed6311be541b80711ab
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2026-07-26 18:24:05 +0900

    Add TestNormal

--- internal/pacorphantest/test.go
+++ internal/pacorphantest/test.go
@@ -40,6 +40,165 @@ func TestEmpty(t *testenv.T) {
 	}
 }
 
+func init() { testMain.Register("TestNormal", TestNormal) }
+func TestNormal(t *testenv.T) {
+	env := testenv.HelpEnv(t)
+
+	srcAExp := testenv.NewPkgBuild("a-explicit", "1.1.1")
+	srcADep1 := testenv.NewPkgBuild("a-dependency-1", "1.2.1")
+	srcADep2 := testenv.NewPkgBuild("a-dependency-2", "1.2.2")
+	srcAOpt1 := testenv.NewPkgBuild("a-optional-1", "1.3.1")
+	srcAOpt2 := testenv.NewPkgBuild("a-optional-2", "1.3.2")
+	srcAExp.Depends = append(srcAExp.Depends, "a-dependency-1")
+	srcAExp.OptDepends = append(srcAExp.OptDepends, "a-optional-1")
+	srcADep1.Depends = append(srcADep1.Depends, "a-dependency-2")
+	srcAOpt1.OptDepends = append(srcAOpt1.OptDepends, "a-optional-2")
+	testenv.HelpMakeAndInstall(t, env, srcAExp, true)
+	testenv.HelpMakeAndInstall(t, env, srcADep1, false)
+	testenv.HelpMakeAndInstall(t, env, srcADep2, false)
+	testenv.HelpMakeAndInstall(t, env, srcAOpt1, false)
+	testenv.HelpMakeAndInstall(t, env, srcAOpt2, false)
+
+	srcBOrp1 := testenv.NewPkgBuild("b-orphan-1", "2.4.1")
+	srcBOrp2 := testenv.NewPkgBuild("b-orphan-2", "2.4.2")
+	srcBOrp3 := testenv.NewPkgBuild("b-orphan-3", "2.4.3")
+	srcBOrp1.Depends = append(srcBOrp1.Depends, "b-orphan-2")
+	srcBOrp1.OptDepends = append(srcBOrp1.OptDepends, "b-orphan-3")
+	testenv.HelpMakeAndInstall(t, env, srcBOrp1, false)
+	testenv.HelpMakeAndInstall(t, env, srcBOrp2, false)
+	testenv.HelpMakeAndInstall(t, env, srcBOrp3, false)
+
+	srcCExp := testenv.NewPkgBuild("c-explicit", "3.1.1")
+	srcCDep := testenv.NewPkgBuild("c-dependency", "3.2.1")
+	srcCOpt1 := testenv.NewPkgBuild("c-optional-1", "3.3.1")
+	srcCOpt2 := testenv.NewPkgBuild("c-optional-2", "3.3.2")
+	srcCExp.Depends = append(srcCExp.Depends, "c-dependency")
+	srcCDep.OptDepends = append(srcCDep.OptDepends, "c-optional-1")
+	srcCOpt1.Depends = append(srcCOpt1.Depends, "c-optional-2")
+	testenv.HelpMakeAndInstall(t, env, srcCExp, true)
+	testenv.HelpMakeAndInstall(t, env, srcCDep, false)
+	testenv.HelpMakeAndInstall(t, env, srcCOpt1, false)
+	testenv.HelpMakeAndInstall(t, env, srcCOpt2, false)
+
+	srcDOrp1 := testenv.NewPkgBuild("d-orphan-1", "4.4.1")
+	srcDOrp2 := testenv.NewPkgBuild("d-orphan-2", "4.4.2")
+	srcDOrp1.Depends = append(srcDOrp1.Depends, "d-orphan-2")
+	srcDOrp2.Depends = append(srcDOrp2.Depends, "d-orphan-1")
+	testenv.HelpMakeAndInstall(t, env, srcDOrp1, false)
+	testenv.HelpMakeAndInstall(t, env, srcDOrp2, false)
+
+	srcEOrp1 := testenv.NewPkgBuild("e-orphan-1", "5.4.1")
+	srcEOrp2 := testenv.NewPkgBuild("e-orphan-2", "5.4.2")
+	srcEOrp1.OptDepends = append(srcEOrp1.OptDepends, "e-orphan-2")
+	srcEOrp2.OptDepends = append(srcEOrp2.OptDepends, "e-orphan-1")
+	testenv.HelpMakeAndInstall(t, env, srcEOrp1, false)
+	testenv.HelpMakeAndInstall(t, env, srcEOrp2, false)
+
+	srcFExp := testenv.NewPkgBuild("f-explicit", "6.1.1")
+	srcFDep1 := testenv.NewPkgBuild("f-dependency-1", "6.2.1")
+	srcFDep2 := testenv.NewPkgBuild("f-dependency-2", "6.2.2")
+	srcFOpt1 := testenv.NewPkgBuild("f-optional-1", "6.3.1")
+	srcFOpt2 := testenv.NewPkgBuild("f-optional-2", "6.3.2")
+	srcFExp.Depends = append(srcFExp.Depends, "f-dependency-1")
+	srcFExp.OptDepends = append(srcFExp.OptDepends, "f-optional-1")
+	srcFDep1.Depends = append(srcFDep1.Depends, "f-dependency-2")
+	srcFDep2.Depends = append(srcFDep2.Depends, "f-dependency-1")
+	srcFOpt1.OptDepends = append(srcFOpt1.OptDepends, "f-optional-2")
+	srcFOpt2.OptDepends = append(srcFOpt2.OptDepends, "f-optional-1")
+	testenv.HelpMakeAndInstall(t, env, srcFExp, true)
+	testenv.HelpMakeAndInstall(t, env, srcFDep1, false)
+	testenv.HelpMakeAndInstall(t, env, srcFDep2, false)
+	testenv.HelpMakeAndInstall(t, env, srcFOpt1, false)
+	testenv.HelpMakeAndInstall(t, env, srcFOpt2, false)
+
+	cmd1 := &exec.Cmd{
+		Path: pacorphan,
+		Args: []string{"pacorphan", "--sysroot", env.Root},
+	}
+
+	stdout1 := new(bytes.Buffer)
+	stderr1 := new(bytes.Buffer)
+	cmd1.Stdout = stdout1
+	cmd1.Stderr = stderr1
+
+	err1 := cmd1.Run()
+	if err1 != nil {
+		t.Error(exiterr.Wrap(cmd1, err1))
+	}
+
+	stdout1Want := strings.Join([]string{
+		"b-orphan-1 2.4.1-1", "b-orphan-2 2.4.2-1", "b-orphan-3 2.4.3-1",
+		"d-orphan-1 4.4.1-1", "d-orphan-2 4.4.2-1",
+		"e-orphan-1 5.4.1-1", "e-orphan-2 5.4.2-1",
+	}, "\n") + "\n"
+	if !bytes.Equal(stdout1.Bytes(), []byte(stdout1Want)) {
+		t.Errorf("exec [%s]: stdout = %q, want %q", strings.Join(cmd1.Args, " "), stdout1.Bytes(), stdout1Want)
+	}
+
+	if stderr1.Len() != 0 {
+		t.Errorf(`exec [%s]: stderr = %q, want ""`, strings.Join(cmd1.Args, " "), stderr1.Bytes())
+	}
+
+	cmd2 := &exec.Cmd{
+		Path: pacorphan,
+		Args: []string{"pacorphan", "--sysroot", env.Root, "--strict"},
+	}
+
+	stdout2 := new(bytes.Buffer)
+	stderr2 := new(bytes.Buffer)
+	cmd2.Stdout = stdout2
+	cmd2.Stderr = stderr2
+
+	err2 := cmd2.Run()
+	if err2 != nil {
+		t.Error(exiterr.Wrap(cmd2, err2))
+	}
+
+	stdout2Want := strings.Join([]string{
+		"a-optional-1 1.3.1-1", "a-optional-2 1.3.2-1",
+		"b-orphan-1 2.4.1-1", "b-orphan-2 2.4.2-1", "b-orphan-3 2.4.3-1",
+		"c-optional-1 3.3.1-1", "c-optional-2 3.3.2-1",
+		"d-orphan-1 4.4.1-1", "d-orphan-2 4.4.2-1",
+		"e-orphan-1 5.4.1-1", "e-orphan-2 5.4.2-1",
+		"f-optional-1 6.3.1-1", "f-optional-2 6.3.2-1",
+	}, "\n") + "\n"
+	if !bytes.Equal(stdout2.Bytes(), []byte(stdout2Want)) {
+		t.Errorf("exec [%s]: stdout = %q, want %q", strings.Join(cmd2.Args, " "), stdout2.Bytes(), stdout2Want)
+	}
+
+	if stderr2.Len() != 0 {
+		t.Errorf(`exec [%s]: stderr = %q, want ""`, strings.Join(cmd2.Args, " "), stderr2.Bytes())
+	}
+
+	cmd3 := &exec.Cmd{
+		Path: pacorphan,
+		Args: []string{"pacorphan", "--sysroot", env.Root, "--quiet"},
+	}
+
+	stdout3 := new(bytes.Buffer)
+	stderr3 := new(bytes.Buffer)
+	cmd3.Stdout = stdout3
+	cmd3.Stderr = stderr3
+
+	err3 := cmd3.Run()
+	if err3 != nil {
+		t.Error(exiterr.Wrap(cmd3, err3))
+	}
+
+	stdout3Want := strings.Join([]string{
+		"b-orphan-1", "b-orphan-2", "b-orphan-3",
+		"d-orphan-1", "d-orphan-2",
+		"e-orphan-1", "e-orphan-2",
+	}, "\n") + "\n"
+	if !bytes.Equal(stdout3.Bytes(), []byte(stdout3Want)) {
+		t.Errorf("exec [%s]: stdout = %q, want %q", strings.Join(cmd3.Args, " "), stdout3.Bytes(), stdout3Want)
+	}
+
+	if stderr3.Len() != 0 {
+		t.Errorf(`exec [%s]: stderr = %q, want ""`, strings.Join(cmd3.Args, " "), stderr3.Bytes())
+	}
+}
+
 func init() { testMain.Register("TestManualPath", TestManualPath) }
 func TestManualPath(t *testenv.T) {
 	// When --sysroot is used, if the DBPath defined in the sysroot's pacman.conf
